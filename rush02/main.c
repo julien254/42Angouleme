06604492493f779c5538b11cacf45270791c35b0
JulienDetre <julien.detre.dev@gmail.com>
2022-01-30 04:12:37 +0100

    finalisation de la plus grosse partie de l'allgo, allgo fonctionnel , reste a ajouter des petite gestion des consignes

--- rush02/main.c
+++ rush02/main.c
@@ -14,6 +14,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 #define SIZE_STR	10000
 
 void	ft_putchar(char c)
@@ -77,9 +78,92 @@ void	init_str(char *str)
 	str[i] = '\0';
 }
 
-void	search_nbr1to20(char *str, char *pattern)
+void stock_nbr_rank_in_temp(char *pattern, char *pattern_temp, int i, int mode)
 {
-	int	i;
+	int j;
+	j = 0;
+	if (mode == 100)
+	{
+	while (j < 3)
+	{
+		if (j == 0)
+			pattern_temp[j] = '1';
+		else
+			pattern_temp[j] = '0';
+		j++;
+	}
+	pattern_temp[j] = '\0';
+	}
+	else
+	{
+		while (pattern[i])
+        {
+                if (j == 0)
+                        pattern_temp[j] = '1';
+                else
+                        pattern_temp[j] = '0';
+                j++;
+                i++;
+        }
+		pattern_temp[j] = '\0';
+	}
+}
+
+void stock_num_in_temp(char *pattern, char *pattern_temp, int index,int rank)
+{
+	int j;
+
+	j = 0;
+        while (j < rank)
+        {
+                if (j == 0)
+		{
+                        pattern_temp[j] = pattern[index];
+			j++;
+			index++;
+		}
+		else
+		{
+			pattern_temp[j] = '0';
+			j++;
+			index++;
+		}
+        }
+	pattern_temp[j] = '\0';
+}
+
+void stock_num_10to19_in_temp(char *pattern, char *pattern_temp, int index,int rank)
+{
+        int j;
+
+        j = 0;
+        while (j < rank)
+        {
+                if (j == 0)
+                {
+                        pattern_temp[j] = pattern[index];
+                        j++;
+                        index++;
+                }
+                else
+                {
+                        pattern_temp[j] = pattern[index];
+                        j++;
+                        index++;
+                }
+        }
+        pattern_temp[j] = '\0';
+}
+
+int check_0_in_rank(char *pattern, int i)
+{
+	if (pattern[i] == '0' && pattern[i - 1] == '0' && pattern[i - 2] == '0')
+		return (0);
+	return (1);
+}
+
+void	search_nbr_in_dico_and_print(char *str, char *pattern, int i)
+{
 	int	temp_i;
 	int	j;
 
@@ -97,7 +181,6 @@ void	search_nbr1to20(char *str, char *pattern)
 			}
 			if (pattern[j] == '\0')
 			{
-				ft_putstr("match trouver\n");
 				while (!(str[temp_i] >= 'a' && str[temp_i] <= 'z'))
 					temp_i++;
 				while (str[temp_i] != '\n')
@@ -105,18 +188,95 @@ void	search_nbr1to20(char *str, char *pattern)
 					ft_putchar(str[temp_i]);
 					temp_i++;
 				}
-					
+				i = -1;				
 			}
 		}
+		if (i == -1)
+			break;
 		i++;
 	}
 }
 
-void	recup_dico(void)
+void	cut_print_pattern(char *str, char *pattern)
 {
-	int	fd;
-	char	*str;
-	int	ret;
+	int i;
+	char *pattern_temp;
+
+	i = 0;
+	pattern_temp = (char*)malloc(sizeof(*str) * SIZE_STR + 1);
+	init_str(pattern_temp);
+	while (pattern[i])
+	{
+		if ((ft_strlen(pattern) - i) % 3 == 1)
+		{
+			
+			if (pattern[i] != '0' && pattern[i - 1] != '1')
+			{
+				stock_num_in_temp(pattern, pattern_temp, i, 1);
+				search_nbr_in_dico_and_print(str, pattern_temp, i);
+				if (i < ft_strlen(pattern) - 3)	
+				{
+					ft_putstr(" ");
+				stock_nbr_rank_in_temp(pattern, pattern_temp, i, 0);
+				search_nbr_in_dico_and_print(str, pattern_temp, i);
+				if (pattern[i + 1] == '0' && (pattern[i + 2] != '0' || pattern[i + 3] != '0'))
+					ft_putstr(", and ");
+				else if (pattern[i + 1] > '0')
+					ft_putstr(", ");
+				}
+				
+			}
+			else if (check_0_in_rank(pattern, i) && i < ft_strlen(pattern) - 3)
+			{
+				stock_nbr_rank_in_temp(pattern, pattern_temp, i, 0);
+                                search_nbr_in_dico_and_print(str, pattern_temp, i);
+				ft_putstr(", ");
+			}
+
+		}
+		else if ((ft_strlen(pattern) - i) % 3 == 2 \
+                                && pattern[i] != '0')
+		{
+			if (pattern[i] != '1')
+			{
+				stock_num_in_temp(pattern, pattern_temp, i, 2);
+				search_nbr_in_dico_and_print(str, pattern_temp, i);
+				if (pattern[i + 1] != '0')
+					ft_putstr("-");
+				else
+					ft_putstr(" ");	
+			}
+			else
+			{
+				stock_num_10to19_in_temp(pattern, pattern_temp, i, 2);
+				search_nbr_in_dico_and_print(str, pattern_temp, i);
+				ft_putstr(" ");
+
+			}
+
+		}
+		
+		else if ((ft_strlen(pattern) - i) % 3 == 0 && pattern[i] != '0')
+		{
+			stock_num_in_temp(pattern, pattern_temp, i, 1);
+                        search_nbr_in_dico_and_print(str, pattern_temp, i);
+				ft_putstr(" ");
+			stock_nbr_rank_in_temp(pattern, pattern_temp, i, 100);
+                        search_nbr_in_dico_and_print(str, pattern_temp, i);
+			if ((pattern[i + 1] != '0' && pattern[i + 2] != '0') || (pattern[i + 1] != '0' || pattern[i + 2] != '0'))
+				ft_putstr(" and ");
+			else
+				ft_putstr(" ");
+                }
+		i++;
+	}
+}
+
+void    recup_dico(char *pattern)
+{
+	int     fd;
+	char    *str;
+	int     ret;
 
 	str = (char*)malloc(sizeof(*str) * SIZE_STR + 1);
 	init_str(str);
@@ -125,22 +285,20 @@ void	recup_dico(void)
 	{
 		ft_putstr("open() failed\n");
 	}
-	ft_putnbr(fd);
-	ft_putchar('\n');
 	ret = read(fd, str, SIZE_STR);
 	str[ret] = '\0';
-	ft_putnbr(ret);
-	ft_putchar('\n');
-	search_nbr1to20(str, "12");
+	cut_print_pattern(str, pattern);
 }
 
 int	main(int argv, char *argc[])
 {
 	if (argv == 2)
 	{
-		ft_putstr(argc[0]);
-		ft_putchar('\0');
-		recup_dico();
+		printf("%s", argc[1]);
+		printf("%c", '\n');
+		recup_dico(argc[1]);
+		printf("%c", '\n');
+
 	}
 	return (0);
-r}
+}
