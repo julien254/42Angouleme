54833170c92873ce168c618edca7e7d07af1257b
julien254 <juliensfr254@gmail.com>
2022-08-09 19:58:59 +0200

    update script debian's install

--- TroncCommun/born2beroot/install-utils.sh
+++ TroncCommun/born2beroot/install-utils.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
+cd ~
 echo "Installation de Git ----->"
 sudo apt install git -y
-echo "Installation de Git termine"
+echo "Installation de Git termine."
+echo "Installation des composant essentiel ----->"
+sudo apt install build-essential python3.9
+echo "Installation des composant essentiel termine."
+echo "Installation de vim 8.2 avec python ----->"
+git clone https://github.com/vim/vim.git
+curl -L https://raw.githubusercontent.com/julien254/42Angouleme/main/ConfigLinux/vimCompilMakefile/Makefile > ~/vim/src/Makefile
+cd ~/vim/src/
+make
+sudo make install
+echo "Installation de vim 8.2 avec python termine."
 echo "Installation de ZSH"
 sudo apt install zsh -y
-echo "Installation de ZSH termine"
-echo "Installation de Oh-My-Zsh"
+echo "Installation de ZSH termine."
+echo "Installation de Oh-My-Zsh ----->"
 sudo apt install curl -y
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-echo "Installation de Oh-My-Zsh termine"
+echo "Installation de Oh-My-Zsh termine."
 echo "Configuration Zsh et vim en cours..."
-cd ~
 git clone https://github.com/julien254/42Angouleme.git
-cd 42Angouleme/TheVim_and_Shell_conf/
+cd 42Angouleme/ConfigLinux/
 cp -r .vim/ ~/
 cp .vimrc ~/
 cp .bashrc ~/
 cp .zshrc ~/
+cd ~
+chsh -s /bin/zsh
