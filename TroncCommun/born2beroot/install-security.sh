dc6bb8c77a02d81b1ac2c2656d6d9c1e0aa0867c
julien254 <juliensfr254@gmail.com>
2022-08-10 01:43:35 +0200

    update script security for born2beroot

--- TroncCommun/born2beroot/install-security.sh
+++ TroncCommun/born2beroot/install-security.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 echo "Installation et configuation du pare-feu UFW..."
-sudo apt install ufw -y && sudo ufw enable && sudo systemctl start ufw && sudo systemctl enable ufw && sudo ufw allow 4242 && sudo ufw allow 22 && sudo systemctl restart ufw
+apt install ufw -y && ufw enable && systemctl start ufw && systemctl enable ufw && ufw allow 4242 && systemctl restart ufw
 echo "Installation et configuration du pare-feu UFW termine."
 echo "Installation et configuration du serveur SSH."
-sudo apt install openssh-server -y
-sudo curl https://raw.githubusercontent.com/julien254/42Angouleme/main/TroncCommun/born2beroot/sshd_config > /etc/ssh/sshd_config
-sudo service ssh restart
+apt install openssh-server -y
+curl https://raw.githubusercontent.com/julien254/42Angouleme/main/TroncCommun/born2beroot/sshd_config > /etc/ssh/sshd_config
+service ssh restart
 echo "Installation et configuration du serveur SSH termine."
 echo "N'oubliez pas de configurer la redirection de port si vous utiliser un VM dans les parametres de celle-ci."
+echo "Si vous n'avez pas lance le script en temp qu'utilisateur root, veillez recommencer l'installation."
