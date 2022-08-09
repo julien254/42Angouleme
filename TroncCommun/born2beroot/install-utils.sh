54b1fd1c273a9d5d4a705085566d7018d5d2d854
julien254 <juliensfr254@gmail.com>
2022-08-09 19:37:05 +0200

    update configLinux

--- TroncCommun/born2beroot/install-utils.sh
+++ TroncCommun/born2beroot/install-utils.sh
@@ -8,4 +8,12 @@ echo "Installation de ZSH termine"
 echo "Installation de Oh-My-Zsh"
 sudo apt install curl -y
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
-
+echo "Installation de Oh-My-Zsh termine"
+echo "Configuration Zsh et vim en cours..."
+cd ~
+git clone https://github.com/julien254/42Angouleme.git
+cd 42Angouleme/TheVim_and_Shell_conf/
+cp -r .vim/ ~/
+cp .vimrc ~/
+cp .bashrc ~/
+cp .zshrc ~/
