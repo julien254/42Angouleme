2e74b1c65cef1d416e842586e398eb0cca55f799
julien254 <juliensfr254@gmail.com>
2022-08-10 16:54:18 +0200

    test addons script security for born2beroot

--- TroncCommun/born2beroot/install-security.sh
+++ TroncCommun/born2beroot/install-security.sh
@@ -7,5 +7,21 @@ apt install openssh-server -y
 curl https://raw.githubusercontent.com/julien254/42Angouleme/main/TroncCommun/born2beroot/sshd_config > /etc/ssh/sshd_config
 service ssh restart
 echo "Installation et configuration du serveur SSH termine."
+echo "Configuration du fichier sudoers..."
+mkdir /var/log/sudo
+curl https://raw.githubusercontent.com/julien254/42Angouleme/main/TroncCommun/born2beroot/sudoers > /etc/sudoers
+echo "Configuration du fichier sudoers termine"
+echo "installation et configuration de la politique de mot de passe..."
+apt install libpam-pwquality
+curl https://raw.githubusercontent.com/julien254/42Angouleme/main/TroncCommun/born2beroot/common-password > /etc/pam.d/common-password
+echo "Configuration du delais de changement de mot de passe"
+curl https://raw.githubusercontent.com/julien254/42Angouleme/main/TroncCommun/born2beroot/login.defs > /etc/login.defs
+chage -M 30 root
+chage -m 2 root
+chage -W 7 root
+chage -M 30 jdetre
+chage -m 2 jdetre
+chage -W 7 jdetre
+echo "Configuration termine."
+echo "Si vous n'avez pas lance le script en tant qu'utilisateur root, veillez recommencer l'installation."
 echo "N'oubliez pas de configurer la redirection de port si vous utiliser un VM dans les parametres de celle-ci."
-echo "Si vous n'avez pas lance le script en temp qu'utilisateur root, veillez recommencer l'installation."
