b9444340bae869702afa4028c78d486b98b19706
julien254 <juliensfr254@gmail.com>
2022-09-02 22:01:38 +0200

    Born2beroot - ajout du script install-bonr2beroot-with-bonus.

--- TroncCommun/born2beroot/install-security.sh
+++ TroncCommun/born2beroot/install-security.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+git clone https://github.com/julien254/42Angouleme.git
 echo "Installation et configuation du pare-feu UFW..."
 apt install ufw -y && ufw enable && systemctl start ufw && systemctl enable ufw && ufw allow 4242 && systemctl restart ufw
 echo "Installation et configuration du pare-feu UFW termine."
@@ -9,19 +10,20 @@ service ssh restart
 echo "Installation et configuration du serveur SSH termine."
 echo "Configuration du fichier sudoers..."
 mkdir /var/log/sudo
-curl https://raw.githubusercontent.com/julien254/42Angouleme/main/TroncCommun/born2beroot/sudoers > /etc/sudoers
+cat 42Angouleme/TroncCommun/born2beroot/sudoers > /etc/sudoers
 echo "Configuration du fichier sudoers termine"
 echo "installation et configuration de la politique de mot de passe..."
 apt install libpam-pwquality -y
-curl https://raw.githubusercontent.com/julien254/42Angouleme/main/TroncCommun/born2beroot/common-password > /etc/pam.d/common-password
-echo "Configuration du delais de changement de mot de passe"
-curl https://raw.githubusercontent.com/julien254/42Angouleme/main/TroncCommun/born2beroot/login.defs > /etc/login.defs
+cat 42Angouleme/TroncCommun/born2beroot/common-password > /etc/pam.d/common-password
+echo "Configuration du delais de changement de mot de passe..."
+cat 42Angouleme/TroncCommun/born2beroot/login.defs > /etc/login.defs
 chage -M 30 root
 chage -m 2 root
 chage -W 7 root
 chage -M 30 jdetre
 chage -m 2 jdetre
 chage -W 7 jdetre
+rm -rf 42Angouleme/
 echo "Configuration termine."
 echo "Si vous n'avez pas lance le script en tant qu'utilisateur root, veillez recommencer l'installation."
 echo "N'oubliez pas de configurer la redirection de port si vous utiliser une VM dans les parametres de celle-ci."
--- TroncCommun/born2beroot/install-utils.sh
+++ TroncCommun/born2beroot/install-utils.sh
@@ -4,13 +4,14 @@ sudo groupadd user42
 sudo usermod -aG user42 jdetre
 echo "Installation de Git ----->"
 sudo apt install git -y
+git clone https://github.com/julien254/42Angouleme.git
 echo "Installation de Git termine."
 echo "Installation des composant essentiel ----->"
 sudo apt install -y build-essential python3.9 python3.9-dev libncurses-dev curl
 echo "Installation des composant essentiel termine."
 echo "Installation de vim 8.2 avec python ----->"
 git clone https://github.com/vim/vim.git
-curl -L https://raw.githubusercontent.com/julien254/42Angouleme/main/ConfigLinux/vimCompilMakefile/Makefile > ~/vim/src/Makefile
+cat 42Angouleme/ConfigLinux/vimCompilMakefile/Makefile > ~/vim/src/Makefile
 cd ~/vim/src/
 make
 sudo make install
@@ -25,12 +26,11 @@ echo "Installation de Oh-My-Zsh ----->"
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 echo "Installation de Oh-My-Zsh termine."
 echo "Configuration Zsh et vim en cours..."
-git clone https://github.com/julien254/42Angouleme.git
 cd 42Angouleme/ConfigLinux/
 cp -r .vim/ ~/
 cp .vimrc ~/
 cp .bashrc ~/
 cp .zshrc ~/
-cd ~ && rm -rf 42Angouleme/
+cd ~
 echo "Configuration Zsh et vim termine"
 echo "rebooter le systeme, puis lancer vim et taper ':PlugInstall' pour finaliser l'installation de vim."
