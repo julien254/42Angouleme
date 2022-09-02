78cf1b16b9b3e14d493c571b84abf8459313158d
julien254 <juliensfr254@gmail.com>
2022-09-02 22:23:34 +0200

    Born2beroot - correction auto yes sur apt install (install-born2beroot-...) .

--- TroncCommun/born2beroot/install-born2beroot-with-bonus.sh
+++ TroncCommun/born2beroot/install-born2beroot-with-bonus.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd ~/
-sudo apt install git
+sudo apt install -y git
 git clone https://github.com/julien254/42Angouleme.git
 sh 42Angouleme/TroncCommun/born2beroot/install-utils.sh
 sudo sh 42Angouleme/TroncCommun/born2beroot/install-security.sh
