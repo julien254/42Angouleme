2e7a1aec3d02600c2fcc8e57eea2e19690daad8d
julien254 <juliensfr254@gmail.com>
2022-09-02 23:44:57 +0200

    born2beroot - test 3 .

--- TroncCommun/born2beroot/install-born2beroot-with-bonus.sh
+++ TroncCommun/born2beroot/install-born2beroot-with-bonus.sh
@@ -5,5 +5,5 @@ git clone https://github.com/julien254/42Angouleme.git
 sh 42Angouleme/TroncCommun/born2beroot/install-utils.sh
 sudo sh 42Angouleme/TroncCommun/born2beroot/install-security.sh
 sh 42Angouleme/TroncCommun/born2beroot/install-wordpress.sh
-sudo rm -rf 42Angouleme/
-echo "Pour terminer l'installation des plugins de vim, lancer l'editeur et taper :PlugInstall "
+echo "Pour finaliser la configuration de mariadb utiliser la commande :"
+echo "sh 42Angouleme/TroncCommun/born2beroot/config-mariadb.sh"
