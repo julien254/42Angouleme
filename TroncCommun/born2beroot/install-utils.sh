974f76e5393171017218f28d08dd93647a13ba40
julien254 <juliensfr254@gmail.com>
2022-09-06 04:02:32 +0200

    Born2beroot - phase de test 1 .

--- TroncCommun/born2beroot/install-utils.sh
+++ TroncCommun/born2beroot/install-utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installation de ZSH"
 sudo apt install zsh -y
-sudo cat ~/42Angouleme/TroncCommun/born2beroot/passwd > /etc/passwd
+sudo cp 42Angouleme/TroncCommun/born2beroot/passwd /etc/passwd
 echo "Installation de ZSH termine."
 cd ~
 sudo groupadd user42
@@ -14,12 +14,6 @@ echo "Installation des composant essentiel ----->"
 sudo apt install -y build-essential python3.9 python3.9-dev libncurses-dev curl
 echo "Installation des composant essentiel termine."
 echo "Installation de vim 8.2 avec python ----->"
-git clone https://github.com/vim/vim.git
-cat 42Angouleme/ConfigLinux/vimCompilMakefile/Makefile > ~/vim/src/Makefile
-cd ~/vim/src/
-make
-sudo make install
-cd ../.. && rm -rf ~/vim/
 echo "Installation de vim 8.2 avec python termine."
 echo "Installation de Oh-My-Zsh ----->"
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
@@ -32,6 +26,7 @@ sudo cp .vimrc /root/
 cp .vimrc ~/
 cp .zshrc ~/
 sudo cp .zshrc /root/
+sudo cp -r ~/.oh-my-zsh /root/
 cd ~
 echo "Configuration Zsh et vim termine"
-echo "lancer vim et taper ':PlugInstall' pour finaliser l'installation des plugins. (Pour le compte utilisateur et le compte ROOT). Vous pouvew ensuite redemarrer le systeme pour aue tous les changements soient pris en compte."
+echo "lancer vim et taper ':PlugInstall' pour finaliser l'installation des plugins. (Pour le compte utilisateur et le compte ROOT). Vous pouvez ensuite redemarrer le systeme pour aue tous les changements soient pris en compte."
