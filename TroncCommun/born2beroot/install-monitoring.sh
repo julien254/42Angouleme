75d8ea5def6da23c012fb5d53913d2ae67747fee
julien254 <juliensfr254@gmail.com>
2022-09-06 14:49:08 +0200

    Born2beroot - phase de test 7 .

--- TroncCommun/born2beroot/install-monitoring.sh
+++ TroncCommun/born2beroot/install-monitoring.sh
@@ -7,5 +7,5 @@ sudo systemctl enable cron
 sudo cp 42Angouleme/TroncCommun/born2beroot/monitoring.sh /root/
 sudo cp 42Angouleme/TroncCommun/born2beroot/sleep.sh /root/
 sudo chmod 755 /root/monitoring.sh /root/sleep.sh
-sudo cp 42Angouleme/TroncCommun/born2beroot/root-crontab.sh /var/spool/cron/crontabs/root
+sudo cp 42Angouleme/TroncCommun/born2beroot/root-crontab /var/spool/cron/crontabs/root
 echo "Monitoring operationel !"
