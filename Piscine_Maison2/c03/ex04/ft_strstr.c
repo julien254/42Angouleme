f5e395cc254afb2b46a0bd2ff9c8643f553b0c37
julien254 <juliensfr254@gmail.com>
2023-07-17 00:31:21 +0200

    ...

--- Piscine_Maison2/c03/ex04/ft_strstr.c
+++ Piscine_Maison2/c03/ex04/ft_strstr.c
@@ -6,28 +6,52 @@
 /*   By: judetre <julien.detre.dev@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 09:27:27 by judetre           #+#    #+#             */
-/*   Updated: 2023/07/15 09:42:33 by judetre          ###   ########.fr       */
+/*   Updated: 2023/07/17 00:30:40 by jdetre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
-
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
+	int	tcheck;
 
 	while (*str)
 	{
 		i = 0;
-		while (to_find[i])
+		tcheck = 1;
+		while (to_find[i] && tcheck)
 		{
 			if (str[i] != to_find[i])
-				break ;
-			i++;
+				tcheck = 0;
+			if (tcheck)
+				i++;
 		}
-		if (to_find[i] == '\0')
+		if (tcheck)
 			return (str);
 		else
 			str++;
 	}
 	return (NULL);
+}
+
+int	main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	char *str;
+	char *str2;
+	str = ft_strstr("salut tous le monde lol" , "mqnde");
+	str2 = strstr("salut tous le monde lol" , "mqnde");
+	if (str == NULL)
+		write(1, "NULL", 4);
+	else
+		printf("%s\n", str);
+	if (str2 == NULL)
+		write(1, "NULL", 4);
+	else
+		printf("%s\n", str2);
+	return (0);
 }
