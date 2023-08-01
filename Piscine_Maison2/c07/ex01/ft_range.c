d534c73a24aa8846203d828de51cd8a009ed6014
Julien Detre <judetre@2G7.42angouleme.fr>
2023-08-01 19:12:04 +0200

    ...

--- Piscine_Maison2/c07/ex01/ft_range.c
+++ Piscine_Maison2/c07/ex01/ft_range.c
@@ -6,22 +6,23 @@
 /*   By: judetre <julien.detre.dev@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 11:51:58 by judetre           #+#    #+#             */
-/*   Updated: 2023/07/25 16:36:50 by judetre          ###   ########.fr       */
+/*   Updated: 2023/07/31 19:00:36 by judetre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
-#include <stddef.h>
 
-int *ft_range(int min, int max)
+int	*ft_range(int min, int max)
 {
 	long long int	size;
-	int	*array;
+	int				*array;
 	long long int	i;
 
 	size = (long long int)max - (long long int)min;
-	if (size < 0)
+	if (size <= 0)
 		return (NULL);
 	array = (int *)malloc(sizeof(int) * size);
+	if (array == NULL)
+		return (NULL);
 	i = 0;
 	while (min < max)
 	{
