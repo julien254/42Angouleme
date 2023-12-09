3a0f97c25437b30f1c5a653939e494c57b1cf38e
julien254 <juliensfr254@gmail.com>
2023-12-10 00:54:36 +0100

    ...

--- TroncCommun/born2beroot/monitoring.sh
+++ TroncCommun/born2beroot/monitoring.sh
@@ -6,7 +6,7 @@ wall -n "#Architecture: $(uname -srvmo)
 #CPU load: $(top -bn1 | grep '^%Cpu' | awk '{printf("%.1f%%", $2 + $4)}')
 #Last boot: $(who -b | awk '{print($3 " " $4)}')
 #LVM use: $(if [ $(lsblk | grep lvm | wc -l ) -eq 0 ]; then echo no; else echo yes; fi)
-#Connection TCP: $(grep TCP /proc/net/sockstat | awk '{print $3}') ESTABLISHED
+#Connection TCP: $(cat /proc/net/sockstat | awk '$1 == "TCP:" {print $3}') ESTABLISHED
 #User log: $(who -u | wc -l)
 #Network: IP $(hostname -I | awk '{print $1}') ($(ip link show | grep link/ether | awk '{print $2}'))
-#Sudo: $(cat /var/log/sudo/sudo.log | grep COMMAND | wc -l) cmd";
+#Sudo: $(strings /var/log/sudo/sudo.log | grep COMMAND | wc -l) cmd";
